e76454b4ae5144cddf0805de74b5d172494e80b9
AFindex <aftouhou@gmail.com>
2022-12-09 03:16:42 +0800

    update : hybrid number

--- lua/KeyMapping/config.lua
+++ lua/KeyMapping/config.lua
@@ -4,8 +4,8 @@ vim.g.mapleader = " "
 local opt = vim.opt
 local keyBindVim = vim
 
-keyBindVim.keymap.set('i','jk','<ESC>')
-keyBindVim.keymap.set("n","<leader>e","$")
+keyBindVim.keymap.set('i', 'jk', '<ESC>')
+keyBindVim.keymap.set("n", "<leader>e", "$")
 
 -- keyBindVim.keymap.set("n","<leader>q","<CMD>:wq<CR>")
 -- keyBindVim.keymap.set("n","<leader>w","<c-w>",{noremap=false})
@@ -20,5 +20,15 @@ keyBindVim.bo.softtabstop = 2
 keyBindVim.wo.number = true
 
 opt.foldlevel = 20
+
+-- hybrid number --
+vim.cmd [[
+  augroup numbertoggle
+    autocmd!
+    autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
+    autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
+  augroup END
+]]
+-- h/t https://jeffkreeftmeijer.com/vim-number/
 -- opt.foldmethod = "expr"
 -- opt.foldexpr = "nvim_treesitter#foldexpr()"
